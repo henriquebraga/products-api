ac8713424746ce6d61a165150f5cf3ac4a731f13
Henrique Braga <henrique.braga@henrique.braga>
2022-05-30 20:56:38 -0300

    create memory db flush function

--- test/db/memory_db_test.clj
+++ test/db/memory_db_test.clj
@@ -2,4 +2,11 @@
   (:require [clojure.test :refer :all])
   (:require [db.memory-db :as db]))
 
+(deftest flush-test
+  (testing "should flush and delete all elements from db"
+    (swap! db/db assoc-in [:category] {:id "EL" :name "Electronics" :slug "/electronics"}))
+    (db/flush-db!)
+    (is (= @db/db {:category {}}))
+  )
+
 
